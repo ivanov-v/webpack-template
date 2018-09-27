d2992937522d4728a70aad1f85f81a443d26190c
ivanov-v <sferamail33@yandex.ru>
2018-09-28 01:00:16 +0300

    базовая конфигурация

---
+++ configs/webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+
+const projectPath = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(projectPath, 'build'),
+    },
+};
